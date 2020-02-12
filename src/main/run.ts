92e1be91a8febbaae8ac6f9b6da63bb6bb3b5aba
Gabriel Handford <gabrielh@gmail.com>
2020-02-12 14:21:32 -0800

    Run in production or env.KEYS_BIN

--- src/main/run.ts
+++ src/main/run.ts
@@ -27,11 +27,21 @@ const execProc = (path: string): Promise<any> => {
 }
 
 export const keysStart = (): Promise<any> => {
-  let keysPath = binPath('bin/keys')
-  if (process.env.KEYS) {
-    keysPath = process.env.KEYS
+  let keysPath = ''
+
+  if (process.env.NODE_ENV === 'production') {
+    keysPath = binPath('bin/keys')
   }
-  return execProc(keysPath + ' start')
+
+  if (process.env.KEYS_BIN) {
+    keysPath = process.env.KEYS_BIN
+  }
+
+  if (keysPath) {
+    return execProc(keysPath + ' start')
+  }
+
+  return Promise.resolve()
 }
 
 const spawnProc = (path: string, killOnExit: boolean): Promise<any> => {
@@ -83,9 +93,9 @@ export const keysd = (): Promise<any> => {
     return spawnProc(servicePath, true)
   }
 
-  console.log('process.env.KEYSD', process.env.KEYSD)
-  if (process.env.KEYSD) {
-    return spawnProc(process.env.KEYSD, true)
+  console.log('process.env.KEYSD', process.env.KEYSD_BIN)
+  if (process.env.KEYSD_BIN) {
+    return spawnProc(process.env.KEYSD_BIN, true)
   }
 
   console.warn('No service spawn in dev mode')
