ed8dc8b2b4259c31712f4784bad5fdf7b689435a
Gabriel Handford <gabrielh@gmail.com>
2020-02-12 18:14:52 -0800

    electron-notarize

---
+++ scripts/notarize.js
@@ -0,0 +1,23 @@
+require('dotenv').config()
+const {notarize} = require('electron-notarize')
+
+exports.default = async function notarizing(context) {
+  const {electronPlatformName, appOutDir} = context
+  if (electronPlatformName !== 'darwin') {
+    return
+  }
+
+  const appName = context.packager.appInfo.productFilename
+
+  // In .env, put APPLEID=...
+  const appleId = process.env.APPLEID
+  // security add-generic-password -a "<appleid>" -w <app_specific_password> -s "AC_PASSWORD"
+  const password = `@keychain:AC_PASSWORD`
+
+  return await notarize({
+    appBundleId: 'pub.Keys',
+    appPath: `${appOutDir}/${appName}.app`,
+    appleId: appleId,
+    appleIdPassword: password,
+  })
+}
