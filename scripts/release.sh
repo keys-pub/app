a934ed7c48811224b3e88c95e33cf8c089ad2667
Gabriel Handford <gabrielh@gmail.com>
2020-03-10 14:26:43 -0700

    Update release.sh

--- scripts/release.sh
+++ scripts/release.sh
@@ -13,7 +13,7 @@ yarn dist
 ver=`yarn run -s version`
 sh scripts/install-release.sh
 
-release fix-build -version $ver -in release/mac/Keys.app -out release
+release fix-build -version $ver -in release -out release
 release latest-yaml -version $ver -in release -out release
 release publish -version $ver -in release
 release cask -version $ver
