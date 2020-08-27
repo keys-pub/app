c0ed9331767eb34cb8aaaa9fd5c3a6bbf1091399
Gabriel Handford <gabrielh@gmail.com>
2020-08-27 11:36:17 -0700

    scripts: Use defaults

--- scripts/release.sh
+++ scripts/release.sh
@@ -28,15 +28,15 @@ fi
 ./scripts/release-tool.sh
 release_bin="$HOME/go/bin/release"
 
-$release_bin download-extra -version $ver -out bin
+$release_bin download-extra -version $ver 
 
 yarn dist
 
 # Notarize (using afterSign notarize.js)
 # sh scripts/notarize.sh $ver    
 
-$release_bin fix-build -version $ver -in release -out release
-$release_bin latest-yaml -version $ver -in release -out release
-$release_bin publish -version $ver -in release
+$release_bin fix-build -version $ver
+$release_bin latest-yaml -version $ver
+$release_bin publish -version $ver
 
 echo "Release finished, see https://github.com/keys-pub/app/releases"
