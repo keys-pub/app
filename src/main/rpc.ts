40e3d59ad8f865029301c1b031e6695ede092ef3
Gabriel Handford <gabrielh@gmail.com>
2020-08-26 21:09:07 -0700

    Update rpc.ts

--- src/main/rpc.ts
+++ src/main/rpc.ts
@@ -50,13 +50,13 @@ export const rpcRegister = () => {
   ipcMain.on('rpc', async (event, arg) => {
     const f = arg as RPC
     const cl = await client(f.service)
-    console.log('rpc', f.reply)
+    // console.log('rpc', f.reply)
     const out: RPCReply = await rpc(cl, f)
     if (out.err) {
       console.error('rpc err', f.reply, out.err)
       handleErr(out.err)
     } else {
-      console.log('rpc reply', f.reply)
+      // console.log('rpc reply', f.reply)
     }
     event.reply(f.reply, out)
   })
@@ -70,26 +70,26 @@ export const rpcRegister = () => {
   ipcMain.on('rpc-stream', async (event, arg) => {
     const f = arg as RPC
     const cl = await client(arg.service)
-    console.log('rpc-stream', f.reply)
+    // console.log('rpc-stream', f.reply)
 
     const stream = streams.get(f.reply)
     if (!!stream) {
-      console.log('found rpc-stream', f.reply)
+      // console.log('found rpc-stream', f.reply)
       if (f.args) {
-        console.log('rpc-stream write', f.reply)
+        // console.log('rpc-stream write', f.reply)
         stream.write(f.args)
       }
       if (f.end) {
-        console.log('rpc-stream end', f.reply)
+        // console.log('rpc-stream end', f.reply)
         stream.end()
         streams.delete(f.reply)
       }
       return
     }
 
-    console.log('new stream', f.reply)
+    // console.log('new stream', f.reply)
     if (typeof cl[f.method] !== 'function') {
-      console.log('rpc-stream not found', f.reply)
+      // console.log('rpc-stream not found', f.reply)
       event.reply(f.reply, {err: notFound(f.method)} as RPCStreamReply)
       return
     }
@@ -98,24 +98,24 @@ export const rpcRegister = () => {
     streams.set(f.reply, newStream)
 
     newStream.on('data', (resp: any) => {
-      console.log('rpc-stream data', f.reply)
+      // console.log('rpc-stream data', f.reply)
       event.reply(f.reply, {resp: resp} as RPCStreamReply)
     })
     newStream.on('error', (err: Error) => {
-      console.log('rpc-stream err', f.reply, err)
+      // console.log('rpc-stream err', f.reply, err)
       handleErr(err)
       event.reply(f.reply, {err: convertErr(err)} as RPCStreamReply)
       streams.delete(f.reply)
     })
     newStream.on('end', () => {
-      console.log('rpc-stream end', f.reply)
+      // console.log('rpc-stream end', f.reply)
       event.reply(f.reply, {done: true} as RPCStreamReply)
       streams.delete(f.reply)
     })
-    console.log('stream write', f.reply)
+    // console.log('stream write', f.reply)
     newStream.write(f.args)
     if (f.end) {
-      console.log('stream end', f.reply)
+      // console.log('stream end', f.reply)
       newStream.end()
     }
   })
